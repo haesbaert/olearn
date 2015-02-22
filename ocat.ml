e316fd4b6d7a30d18c292b1ecbb0bfa78d416270
Christiano F. Haesbaert <haesbaert@haesbaert.org>
2015-02-22 17:36:49 +0100

    Add ocat -n flag.

--- ocat.ml
+++ ocat.ml
@@ -3,29 +3,42 @@ open Core.Std
 let buf_size = 4096
 
 let rec copyloop ic oc buf =
-  let c = In_channel.input ic ~pos:0 ~buf:buf ~len:buf_size in
-  match c with
+  let c = In_channel.input ic ~pos:0 ~buf:buf ~len:buf_size
+  in match c with
   | 0 -> ()
   | _ -> Out_channel.output oc ~pos:0 ~buf:buf ~len:c;
     copyloop ic oc buf
 
-let do_cat file =
+let rec copylines ic oc buf linenum =
+  let line = In_channel.input_line ic
+  in match line with
+    | None -> ()
+    | Some line -> Out_channel.output_string oc (sprintf "%6d  %s\n" linenum line);
+      copylines ic oc buf (linenum + 1)
+
+let do_cat file nflag =
+  let oc = Out_channel.stdout in
   let ic = match file with
-  | None | Some "-" -> In_channel.stdin
-  | Some file -> In_channel.create file in
-  copyloop ic Out_channel.stdout (Bytes.create buf_size);
+    | "-" -> In_channel.stdin
+    | file -> In_channel.create file
+  in
+  let () = match nflag with
+    | false -> copyloop ic oc (Bytes.create buf_size)
+    | true  -> copylines ic oc (Bytes.create buf_size) 1
+  in
   In_channel.close ic
 
 let spec =
   let open Command.Spec in
   empty
-  +> anon (maybe ("filename" %: string))
+  +> flag "-n" no_arg ~doc:" print line numbers"
+  +> anon (maybe_with_default "-" ("filename" %: string))
 
 let command =
   Command.basic
     ~summary:"basic cat implementation."
     spec
-    (fun filename () -> do_cat filename)
+    (fun nflag filename () -> do_cat filename nflag)
 
 let () =
   Command.run ~version:"1.0" command
