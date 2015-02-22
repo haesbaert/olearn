c4aa18c4f64a3796c275f83788a665bfc9883a7d
Christiano F. Haesbaert <haesbaert@haesbaert.org>
2015-02-22 19:39:22 +0100

    Add -b option, escape blank lines

--- ocat.ml
+++ ocat.ml
@@ -11,16 +11,24 @@ let rec copyloop ic oc buf uflag =
       Out_channel.flush oc;
     copyloop ic oc buf uflag
 
-let rec copylines ic oc buf linenum uflag =
+let escapeline line bflag =
+  match bflag with
+  | false -> false
+  | true -> String.length line = 0
+
+let rec copylines ic oc buf linenum uflag bflag =
   let line = In_channel.input_line ic
   in match line with
     | None -> ()
-    | Some line -> Out_channel.output_string oc (sprintf "%6d  %s\n" linenum line);
-      if uflag = true then
-        Out_channel.flush oc;
-      copylines ic oc buf (linenum + 1) uflag
+    | Some line -> match escapeline line bflag with
+      | true -> copylines ic oc buf linenum uflag bflag
+      | false -> Out_channel.output_string oc (sprintf "%6d  %s\n" linenum line);
+        if uflag = true then
+          Out_channel.flush oc;
+        copylines ic oc buf (linenum + 1) uflag bflag
 
-let do_cat file nflag uflag =
+let do_cat file nflag uflag bflag =
+  let nflag = if bflag = true then true else nflag in
   let oc = Out_channel.stdout in
   let buf = Bytes.create buf_size in
   let ic = match file with
@@ -29,13 +37,14 @@ let do_cat file nflag uflag =
   in
   let () = match nflag with
     | false -> copyloop ic oc buf uflag
-    | true  -> copylines ic oc buf 1 uflag
+    | true  -> copylines ic oc buf 1 uflag bflag
   in
   In_channel.close ic
 
 let spec =
   let open Command.Spec in
   empty
+  +> flag "-b" no_arg ~doc:" escape empty lines, implies -n"
   +> flag "-n" no_arg ~doc:" print line numbers"
   +> flag "-u" no_arg ~doc:" unbuffered output"
   +> anon (maybe_with_default "-" ("filename" %: string))
@@ -44,7 +53,7 @@ let command =
   Command.basic
     ~summary:"basic cat implementation."
     spec
-    (fun nflag uflag filename () -> do_cat filename nflag uflag)
+    (fun bflag nflag uflag filename () -> do_cat filename nflag uflag bflag)
 
 let () =
   Command.run ~version:"1.0" command
