8bb6c72d8e10309e0f05633d1dcce897dc669305
Christiano F. Haesbaert <haesbaert@haesbaert.org>
2015-03-01 14:51:56 +0100

    Support multiple files, including intermixed stdin.

--- ocat.ml
+++ ocat.ml
@@ -17,61 +17,67 @@ let toascii c =
   | true ->  (char_of_int ((int_of_char c) land 0o177))
   | false -> c
 
-let cat bflag eflag nflag sflag tflag uflag vflag file =
+let catfiles bflag eflag nflag sflag tflag uflag vflag files =
   let nflag = bflag || nflag in
   let vflag = eflag || tflag || vflag in
   let ochar = output_char stdout in
   let ostr = output_string stdout in
   let bufsize = 1024 * 8 in
-  let ic = match file with
-    | "-" -> stdin
-    | file -> open_in file
+  let cat file =
+    let ic = match file with
+      | "-" -> stdin
+      | file -> open_in file
+    in
+    let rec catloop ic lnum c prev gobble =
+      let ignline = sflag && gobble && c = '\n' && prev = '\n' in
+      let gobble = prev = '\n' && c = '\n' in
+      match ignline with
+      | true -> catloop ic lnum (input_char ic) c gobble
+      | _ ->
+        let supbln = bflag && c = '\n' && prev = '\n' in (* supress blanks *)
+        (* Should we print the line preamble *)
+        if nflag && prev = '\n' && not supbln then
+          ostr (Printf.sprintf "%6d\t" lnum);
+        let () = match vflag, isascii c, (iscntrl c) with
+          | true, false, true ->
+            ostr "M-^";
+            ochar (cntrlcharv c);
+            ochar (toascii c)
+          | true, false, false ->
+            ostr "M-";
+            ochar (toascii c)
+          | _ -> match c, eflag, tflag with
+            | '\n', true, _    -> ostr "$\n"
+            | '\t', _,    true -> ostr "^I"
+            | _ -> ochar c
+        in
+        (* Should we increase the line number *)
+        let nlnum = match c = '\n', supbln with
+          | false, _ -> lnum
+          | true, true -> lnum
+          | true, false -> succ lnum
+        in
+        if uflag then flush stdout;
+        catloop ic nlnum (input_char ic) c gobble
+    in
+    let rec rawcatloop ic buf =
+      match input ic buf 0 bufsize with
+      | 0 -> ()
+      | len -> output stdout buf 0 len;
+        if uflag then flush stdout;
+        rawcatloop ic buf
+    in
+    try
+      match bflag, nflag, vflag, sflag with
+      | false, false, false, false ->
+        rawcatloop ic (Bytes.create bufsize)
+      | _ ->
+        catloop ic 1 (input_char ic) '\n' false
+    with End_of_file -> if ic <> stdin then close_in ic
   in
-  let rec catloop ic lnum c prev gobble =
-    let ignline = sflag && gobble && c = '\n' && prev = '\n' in
-    let gobble = prev = '\n' && c = '\n' in
-    match ignline with
-    | true -> catloop ic lnum (input_char ic) c gobble
-    | _ ->
-      let supbln = bflag && c = '\n' && prev = '\n' in (* supress blanks *)
-      (* Should we print the line preamble *)
-      if nflag && prev = '\n' && not supbln then
-        ostr (Printf.sprintf "%6d\t" lnum);
-      let () = match vflag, isascii c, (iscntrl c) with
-        | true, false, true ->
-          ostr "M-^";
-          ochar (cntrlcharv c);
-          ochar (toascii c)
-        | true, false, false ->
-          ostr "M-";
-          ochar (toascii c)
-        | _ -> match c, eflag, tflag with
-          | '\n', true, _    -> ostr "$\n"
-          | '\t', _,    true -> ostr "^I"
-          | _ -> ochar c
-      in
-      (* Should we increase the line number *)
-      let nlnum = match c = '\n', supbln with
-        | false, _ -> lnum
-        | true, true -> lnum
-        | true, false -> succ lnum
-      in
-      if uflag then flush stdout;
-      catloop ic nlnum (input_char ic) c gobble
-  in
-  let rec rawcatloop ic buf =
-    match input ic buf 0 bufsize with
-    | 0 -> ()
-    | len -> output stdout buf 0 len;
-      if uflag then flush stdout;
-      rawcatloop ic buf
-  in
-  try
-    match bflag, nflag, vflag, sflag with
-    | false, false, false, false -> rawcatloop ic (Bytes.create bufsize)
-    | _ -> catloop ic 1 (input_char ic) '\n' false
-  with End_of_file -> ()
-
+  (* cat main body *)
+  List.iter cat files
+                        
 (* Build Cmdliner command parser *)
 open Cmdliner
 let bflag = Arg.(value & flag & info ["b"] ~doc:"Escape empty lines, implies -n.")
@@ -81,11 +87,11 @@ let sflag = Arg.(value & flag & info ["s"] ~doc:"Squeeze adjacent newlines")
 let tflag = Arg.(value & flag & info ["t"] ~doc:"Implies -v, and display tabs as ^I")
 let uflag = Arg.(value & flag & info ["u"] ~doc:"Print line numbers")
 let vflag = Arg.(value & flag & info ["v"] ~doc:"Unbuffered output")
-let file = Arg.(value & pos 0 string "-" & info [] ~docv:"FILENAME")
-
+(* let files = Arg.(value & pos 0 string "-" & info [] ~docv:"FILENAME") *)
+let files = Arg.(value & (pos_all string []) & info [] ~docv:"FILES")
 let cmd =
   let doc = "cat clone in ocaml" in
-  Term.(pure cat $ bflag $ eflag $ nflag $ sflag $ tflag $ uflag $ vflag $ file),
+  Term.(pure catfiles $ bflag $ eflag $ nflag $ sflag $ tflag $ uflag $ vflag $ files),
   Term.info "cat" ~version:"1.1" ~doc
 
 let () = match Term.eval cmd with `Error _ -> exit 1 | _ -> exit 0
